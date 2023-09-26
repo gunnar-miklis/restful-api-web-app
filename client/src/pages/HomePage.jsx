b541f2607ed19ec4ccaa6b6a48dfea997ab21399
Gunnar Miklis <gunnar.miklis@gmail.com>
2023-09-26 14:34:26 +0200

    update

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
 			<h1>Home</h1>
 
 			<p style={{ marginBottom: 0 }}>RESTful API Web App</p>
-			<p style={{ marginTop: 0, fontSize: 'small' }}>by <a href='https://github.com/gunnar-miklis' target='_blank' rel="noreferrer">Gunnar Miklis</a></p>
+			<p style={{ marginTop: 0, fontSize: 'small' }}>by <a href='https://flowcv.me/gunnar-miklis' target='_blank' rel="noreferrer">Gunnar Miklis</a></p>
 
 			<br/>
 			<NavLink to='/todolists'>Show Todolists</NavLink>
